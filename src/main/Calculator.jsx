d2734be9ddfaa7629cc2c20df9137259062464e7
flaviospjunior <flaviospjunior@gmail.com>
2021-08-01 17:06:26 -0300

    Commit inicial

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./Calculator.css";
+import Button from "../components/Button";
+
+export default class Calculator extends Component {
+  render() {
+    return (
+      <div className="calculator">
+        <Button label="AC"></Button>
+        <Button></Button>
+        <Button></Button>
+        <Button></Button>
+        <Button></Button>
+        <Button></Button>
+      </div>
+    );
+  }
+}
